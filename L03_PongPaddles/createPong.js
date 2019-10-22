238960ede225cacf52bd825d2166eb9414f0f4a9
Sarah <johndoe@example.com>
2019-10-22 17:14:13 +0200

    pong Anfang

--- L03_PongPaddles/createPong.js
+++ L03_PongPaddles/createPong.js
@@ -2,24 +2,27 @@
 var L03_PongPaddels;
 (function (L03_PongPaddels) {
     var f = FudgeCore;
+    L03_PongPaddels.ball = new f.Node("Ball");
+    L03_PongPaddels.pLeft = new f.Node("pLeft");
+    L03_PongPaddels.pRight = new f.Node("pRight");
     function createPong() {
         let pong = new f.Node("Pong");
-        let ball = new f.Node("Ball");
-        let pLeft = new f.Node("pLeft");
-        let pRight = new f.Node("pRight");
         let mesh = new f.MeshQuad();
         //let cmpMesh: f.ComponentMesh = new f.ComponentMesh(mesh);
         let mtrSolidWhite = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(new f.Color(1, 1, 1, 1)));
         //let cmpMaterial: f.ComponentMaterial = new f.ComponentMaterial(mtrSolidWhite);
-        ball.addComponent(new f.ComponentMesh(mesh));
-        ball.addComponent(new f.ComponentMaterial(mtrSolidWhite));
-        pLeft.addComponent(new f.ComponentMesh(mesh));
-        pLeft.addComponent(new f.ComponentMaterial(mtrSolidWhite));
-        pRight.addComponent(new f.ComponentMesh(mesh));
-        pRight.addComponent(new f.ComponentMaterial(mtrSolidWhite));
-        pong.appendChild(ball);
-        pong.appendChild(pLeft);
-        pong.appendChild(pRight);
+        L03_PongPaddels.ball.addComponent(new f.ComponentMesh(mesh));
+        L03_PongPaddels.ball.addComponent(new f.ComponentMaterial(mtrSolidWhite));
+        L03_PongPaddels.ball.addComponent(new f.ComponentTransform());
+        L03_PongPaddels.pLeft.addComponent(new f.ComponentMesh(mesh));
+        L03_PongPaddels.pLeft.addComponent(new f.ComponentMaterial(new f.Material("LightBlue", f.ShaderUniColor, new f.CoatColored(new f.Color(.6, .6, 1, 1)))));
+        L03_PongPaddels.pLeft.addComponent(new f.ComponentTransform());
+        L03_PongPaddels.pRight.addComponent(new f.ComponentMesh(mesh));
+        L03_PongPaddels.pRight.addComponent(new f.ComponentMaterial(new f.Material("lightRed", f.ShaderUniColor, new f.CoatColored(new f.Color(1, .3, .3, 1)))));
+        L03_PongPaddels.pRight.addComponent(new f.ComponentTransform());
+        pong.appendChild(L03_PongPaddels.ball);
+        pong.appendChild(L03_PongPaddels.pLeft);
+        pong.appendChild(L03_PongPaddels.pRight);
         return pong;
     }
     L03_PongPaddels.createPong = createPong;
--- L03_PongPaddles/main.ts
+++ L03_PongPaddles/main.ts
@@ -10,7 +10,7 @@ namespace L03_PongPaddels {
     function init(_evevt: Event): void {
         let canvas: HTMLCanvasElement = document.querySelector("canvas");
 
-        console.log(canvas);
+        //console.log(canvas);
 
         f.RenderManager.initialize();
         f.Debug.log(canvas);
@@ -18,14 +18,23 @@ namespace L03_PongPaddels {
         let pong: f.Node = createPong();
 
         let cmpCamera: f.ComponentCamera = new f.ComponentCamera();
-        cmpCamera.pivot.translateZ(15);
+        cmpCamera.pivot.translateZ(42);
+
+        let cmpTransform: f.ComponentTransform = pRight.getComponent(f.ComponentTransform);
+
+        pRight.cmpTransform.local.translateX(20);
+        //pRight.cmpTransform.local.scaleY(5);
+        ( <f.ComponentMesh> pRight.getComponent(f.ComponentMesh) ).pivot.scaleY(5);
+
+        pLeft.cmpTransform.local.translateX(-20);
+        //pLeft.cmpTransform.local.scaleY(5);
+        ( <f.ComponentMesh> pLeft.getComponent(f.ComponentMesh) ).pivot.scaleY(5);
+        console.log(cmpTransform);
 
         viewport = new f.Viewport();
         viewport.initialize("Viewport", pong, cmpCamera, canvas);
         f.Debug.log(viewport);
         
         viewport.draw();
-
     }
-
 }
