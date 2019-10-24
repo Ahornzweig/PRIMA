9a078b0a295a8a946093c6bc9d282ff0d9d09a64
Sarah <johndoe@example.com>
2019-10-24 15:34:15 +0200

    test

--- L03_PongPaddles/main.js
+++ L03_PongPaddles/main.js
@@ -5,6 +5,7 @@ var L03_PongPaddels;
 (function (L03_PongPaddels) {
     var f = FudgeCore;
     window.addEventListener("load", init);
+    let viewport;
     function init(_evevt) {
         let canvas = document.querySelector("canvas");
         //console.log(canvas);
@@ -13,18 +14,65 @@ var L03_PongPaddels;
         let pong = L03_PongPaddels.createPong();
         let cmpCamera = new f.ComponentCamera();
         cmpCamera.pivot.translateZ(42);
-        let cmpTransform = L03_PongPaddels.pRight.getComponent(f.ComponentTransform);
+        //let cmpTransform: f.ComponentTransform = pRight.getComponent(f.ComponentTransform);
+        //console.log(cmpTransform);
         L03_PongPaddels.pRight.cmpTransform.local.translateX(20);
         //pRight.cmpTransform.local.scaleY(5);
         L03_PongPaddels.pRight.getComponent(f.ComponentMesh).pivot.scaleY(5);
         L03_PongPaddels.pLeft.cmpTransform.local.translateX(-20);
         //pLeft.cmpTransform.local.scaleY(5);
         L03_PongPaddels.pLeft.getComponent(f.ComponentMesh).pivot.scaleY(5);
-        console.log(cmpTransform);
-        L03_PongPaddels.viewport = new f.Viewport();
-        L03_PongPaddels.viewport.initialize("Viewport", pong, cmpCamera, canvas);
-        f.Debug.log(L03_PongPaddels.viewport);
-        L03_PongPaddels.viewport.draw();
+        viewport = new f.Viewport();
+        viewport.initialize("Viewport", pong, cmpCamera, canvas);
+        f.Debug.log(viewport);
+        viewport.draw();
+        document.addEventListener("keydown", down);
+        document.addEventListener("keyup", up);
+        f.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        f.Loop.start();
+    }
+    let keysPressed = {};
+    function down(_evevt) {
+        let keyCode = _evevt.keyCode;
+        keysPressed[keyCode] = true;
+        /*switch (keyCode) {
+            case 38: //up
+                //pRight.cmpTransform.local.translateY(.5);
+                break;
+
+            case 40: //down
+                //pRight.cmpTransform.local.translate(new f.Vector3(0, -.5, 0));
+                break;
+
+            case 87: //w
+                //pLeft.cmpTransform.local.translate(f.Vector3.Y(.5));
+                break;
+
+            case 83: //s
+                //pLeft.cmpTransform.local.translateY(-.5);
+                break;
+        }*/
+    }
+    function up(_evevt) {
+        let keyCode = _evevt.keyCode;
+        keysPressed[keyCode] = false;
+    }
+    function update(_evevt) {
+        //f.Debug.log("update");
+        if (keysPressed[38] == true) {
+            L03_PongPaddels.pRight.cmpTransform.local.translateY(.5);
+        }
+        else if (keysPressed[40] == true) {
+            L03_PongPaddels.pRight.cmpTransform.local.translate(new f.Vector3(0, -.5, 0));
+        }
+        if (keysPressed[87] == true) {
+            L03_PongPaddels.pLeft.cmpTransform.local.translate(f.Vector3.Y(.5));
+        }
+        else if (keysPressed[83] == true) {
+            L03_PongPaddels.pLeft.cmpTransform.local.translate(new f.Vector3(0, -.5, 0));
+        }
+        f.RenderManager.update();
+        viewport.draw();
     }
 })(L03_PongPaddels || (L03_PongPaddels = {}));
 //# sourceMappingURL=main.js.map
